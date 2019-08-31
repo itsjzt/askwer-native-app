882a57856636e5f1e6a95cc81b3df56eee7d4c16
dhruv <dhruvsach18gmaill.com@dhruvs-MacBook-Pro.local>
2019-09-01 00:58:24 +0530

    Added react navigation

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import QuestionSingle from "./pages/QuestionSingle";
+import { createAppContainer } from "react-navigation";
+import { Navigation } from "./navigation/routes";
 
 export interface AppProps {}
 
 // todo: add react native navigation
 const App: React.SFC<AppProps> = () => {
-  return <QuestionSingle />;
+  return <Nav />;
 };
-
+const Nav = createAppContainer(Navigation);
 export default App;
