5e6ff84f14c0f985fbf982df32b79fdebce43392
Saurabh Sharma <saurabh.19@outlook.com>
2019-09-01 12:09:19 +0530

    removed appNavigator for Now

--- navigation/routes.tsx
+++ navigation/routes.tsx
@@ -1,11 +1,9 @@
 import { createSwitchNavigator } from "react-navigation";
-import AppNavigator from "./AppNavigator";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
 import Feeds from "../pages/Feeds";
 
 const routes = {
-  AppNavigator: AppNavigator,
   Login: Login,
   Signup: Signup,
   Feeds: Feeds,
--- pages/Feeds.tsx
+++ pages/Feeds.tsx
@@ -1,10 +1,16 @@
 import * as React from "react";
 import { View, StyleSheet } from "react-native";
+import PostCard from "../components/QuestionCard";
+import { postMock } from "../utils/mock";
 
 export interface FeedsProps {}
 
 const Feeds: React.SFC<FeedsProps> = () => {
-  return <View style={styles.container}>Hello World</View>;
+  return (
+    <View style={styles.container}>
+      <PostCard {...postMock} />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
