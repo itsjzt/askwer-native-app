d053357ea770dc77e9a5532bce6113ea5b576d52
Saurabh Sharma <saurabh.19@outlook.com>
2019-09-01 21:40:03 +0530

    added tabBar

--- navigation/routes.tsx
+++ navigation/routes.tsx
@@ -1,9 +1,10 @@
-import { createSwitchNavigator } from "react-navigation";
+// import { createSwitchNavigator } from "react-navigation";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
 import QuestionSingle from "../pages/QuestionSingle";
 import Feeds from "../pages/Feeds";
-import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import { getTabBarIcon } from "../components/getTabBarIcon";
 
 const routes = {
   QuestionSingle: QuestionSingle,
@@ -13,15 +14,20 @@ const routes = {
 };
 
 const options = {
-  initialRouteName: "QuestionSingle",
-  defaultNavigationOptions: {
-    headerStyle: {
-      display: "none",
-    },
+  initialRouteName: "Feeds",
+  defaultNavigationOptions: ({ navigation }) => ({
+    tabBarIcon: ({ focused, tintColor }) =>
+      getTabBarIcon(navigation, focused, tintColor),
+  }),
+  tabBarOptions: {
+    activeTintColor: "tomato",
+    inactiveTintColor: "gray",
   },
 };
+
+export const Navigation = createBottomTabNavigator(routes, options);
 
 // @ts-ignore
 // this is a ts error because string is not assinable to "none"|"flex"
 // this is why you can hate strict types
-export const Navigation = createStackNavigator(routes, options);
+// export const Navigation = createStackNavigator(routes, options);
