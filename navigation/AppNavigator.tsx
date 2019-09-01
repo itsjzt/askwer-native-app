154a22af9e681fc44c15fee14b6599fd5334afea
Saurabh Sharma <saurabh.19@outlook.com>
2019-09-01 12:07:39 +0530

    refactor

--- navigation/AppNavigator.tsx
+++ navigation/AppNavigator.tsx
@@ -2,13 +2,13 @@ import { createStackNavigator } from "react-navigation-stack";
 import Feeds from "../pages/Feeds";
 export default createStackNavigator(
   {
-    Feeds: Feeds
+    Feeds: Feeds,
   },
   {
     defaultNavigationOptions: {
       headerStyle: {
-        display: "none"
-      }
-    }
+        display: "none",
+      },
+    },
   }
 );
--- navigation/routes.tsx
+++ navigation/routes.tsx
@@ -2,13 +2,17 @@ import { createSwitchNavigator } from "react-navigation";
 import AppNavigator from "./AppNavigator";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
-export const Navigation = createSwitchNavigator(
-  {
-    AppNavigator: AppNavigator,
-    Login: Login,
-    Signup: Signup
-  },
-  {
-    initialRouteName: "Login"
-  }
-);
+import Feeds from "../pages/Feeds";
+
+const routes = {
+  AppNavigator: AppNavigator,
+  Login: Login,
+  Signup: Signup,
+  Feeds: Feeds,
+};
+
+const intialRoute = {
+  initialRouteName: "Login",
+};
+
+export const Navigation = createSwitchNavigator(routes, intialRoute);
